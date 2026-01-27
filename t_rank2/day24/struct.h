146bf3df24694df00937c3e695b3705b0712ba42
Peazeved24 <peazeved@student.42lisboa.com>
2026-01-27 16:14:17 +0000

    W

--- t_rank2/day24/struct.h
+++ t_rank2/day24/struct.h
@@ -1,40 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   max.c                                              :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: peazeved <peazeved@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/25 15:30:47 by peazeved          #+#    #+#             */
-/*   Updated: 2025/10/25 15:37:44 by peazeved         ###   ########.fr       */
+/*   Created: 2025/12/08 15:52:20 by peazeved          #+#    #+#             */
+/*   Updated: 2025/12/08 16:27:21 by peazeved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "struct.h"
+#ifndef STRUCT_H
+#define STRUCT_H
 
-int ft_cmp(int n1, int n2)
-{
-    return (n1 - n2);
-}
 
-int max(int *tab , unsigned int len)
-{
-    int i =  0;
-    int tmp = tab[i];
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-    while(i < len)
-    {
-        if(ft_cmp(tab[i], tmp) > 0)
-            tmp = tab[i];
-        i++;
-    }
-    return tmp;
-}
-
-int main()
-{
-    int arr[] = {10, 43, 90, 292, 2};
-    unsigned int size = 5;
-    printf("%i", max(arr, size));
-    return 0;
-}
+#endif
